63f8b24fc2ab1e4c5cfefc77bd736981c3218507
Mr. Chip53 <support@freebrew.org>
2023-01-31 19:22:15 -0600

    Add project files.

---
+++ SuperTube/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperTube;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SuperTube/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperTube;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
